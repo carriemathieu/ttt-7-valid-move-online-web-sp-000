79e43fcf537ceb6d11aa4cb66b5df0bc9f60fe31
Carrie Mathieu <carrieburks@gmail.com>
2020-08-06 19:59:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here - true if present on game board and not already filled
 def valid_move?(board, index)
+<<<<<<< HEAD
   if index < 0 || index > 9 
     FALSE
   elsif index >= 0 && index <= 9
@@ -13,6 +14,14 @@ end
 
 # returns true for a valid position on an empty board
 # returns nil or false for an occupied position
+=======
+  if index < 1 || index > 8
+    FALSE
+  elsif index >= 1 && index <= 8 
+    TRUE
+  end   
+end
+>>>>>>> a64f6e23e993af20ba43b11cbb854ee280d09f25
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
